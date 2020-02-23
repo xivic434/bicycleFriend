c15041fd476a1cc967ee8690fdbb02d5b6df1781
xivic434 <xivic434@gmail.com>
2020-02-24 01:55:08 +0900

    회원가입메뉴 작업.

--- src/components/memberModules/joinSteps/confirmUserSign.js
+++ src/components/memberModules/joinSteps/confirmUserSign.js
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 
 
 /**
- * 회원가입 1단계 
+ * 회원가입 1단계 - 개인정보 취급방침 관련 . 사용자의 승인을 받는다.
  */
 export default class ConfirmUserSign extends Component {
     constructor(props){
@@ -11,7 +11,19 @@ export default class ConfirmUserSign extends Component {
 
     render () {
         return (
+            <>
             <div>Step 1</div>
+            <section>
+                <dl>
+                    <dt>개인정보 취급방침</dt>
+                    <dd>우리는 어쩌구저쩌구 다 허용할꺼야</dd>
+                </dl>
+                <label >
+                    허용 한다.
+                    <input type="checkbox"></input>
+                </label>
+            </section>
+            </>
         )
     }
 }
--- src/components/memberModules/joinSteps/registUserInfo.js
+++ src/components/memberModules/joinSteps/registUserInfo.js
@@ -1,5 +1,8 @@
 import React, {Component} from "react";
 
+/**
+ * 회원가입 2단계 - 사용자 기본 정보를 입력받는 곳 .
+ */
 export default class RegistUserInfo extends Component {
     constructor(props){
         super(props);
@@ -7,7 +10,12 @@ export default class RegistUserInfo extends Component {
 
     render () { 
         return (
+            <>
             <div>Step 2 </div>
+            <div> 
+                
+            </div>
+            </>
         )
     }
 }
